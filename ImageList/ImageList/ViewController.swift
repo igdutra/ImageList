ead815a69b558cb5bcf154cca8bb8fce2c7dcc9b
Ivo Dutra <ivogdutra@gmail.com>
2020-08-04 15:49:48 -0300

    created proj

---
+++ ImageList/ImageList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageList
+//
+//  Created by Ivo Dutra on 04/08/20.
+//  Copyright © 2020 Ivo Dutra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
